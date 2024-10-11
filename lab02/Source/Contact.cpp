d3d42a6f3a8d8ee43a297a4a89c78a1bcc44623e
eofid <arhipadynec@gmail.com>
2024-10-11 15:13:14 +0300

    Contact.cpp

--- lab02/Source/Contact.cpp
+++ lab02/Source/Contact.cpp
@@ -4,8 +4,21 @@
 Contact::Contact(const std::string& name, const std::string& phone, const std::string& email)
     : name(name), phone(phone), email(email) {}
 
-// Реализация скрытого друга — оператора <<
+// Реализация перегрузки оператора "=="
+bool operator==(const Contact& lhs, const Contact& rhs) {
+    return (lhs.name == rhs.name) && (lhs.phone == rhs.phone) && (lhs.email == rhs.email);
+}
+
+// Реализация оператора вывода (<<) для записи в поток
 std::ostream& operator<<(std::ostream& os, const Contact& contact) {
     os << contact.name << "\n" << contact.phone << "\n" << contact.email << "\n";
     return os;
+}
+
+// Реализация оператора ввода (>>) для чтения из потока
+std::istream& operator>>(std::istream& is, Contact& contact) {
+    std::getline(is, contact.name);
+    std::getline(is, contact.phone);
+    std::getline(is, contact.email);
+    return is;
 }
