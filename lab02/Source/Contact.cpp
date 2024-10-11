af23e952c1b5eec896e18528ece177aefd908b4e
eofid <arhipadynec@gmail.com>
2024-10-11 11:22:09 +0300

    Contact.cpp

--- lab02/Source/Contact.cpp
+++ lab02/Source/Contact.cpp
@@ -1,16 +1,11 @@
 #include "Contact.h"
 
-// Реализация конструктора Contact
+// Р РµР°Р»РёР·Р°С†РёСЏ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°
 Contact::Contact(const std::string& name, const std::string& phone, const std::string& email)
     : name(name), phone(phone), email(email) {}
 
-// Реализация перегрузки оператора "=="
-bool operator==(const Contact& lhs, const Contact& rhs) {
-    return (lhs.name == rhs.name) && (lhs.phone == rhs.phone) && (lhs.email == rhs.email);
-}
-
-// Реализация оператора вывода (для записи в файл)
+// Р РµР°Р»РёР·Р°С†РёСЏ СЃРєСЂС‹С‚РѕРіРѕ РґСЂСѓРіР° вЂ” РѕРїРµСЂР°С‚РѕСЂР° <<
 std::ostream& operator<<(std::ostream& os, const Contact& contact) {
     os << contact.name << "\n" << contact.phone << "\n" << contact.email << "\n";
-    return os;  // Возвращаем поток
+    return os;
 }
