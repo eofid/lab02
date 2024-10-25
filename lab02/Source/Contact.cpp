7b8c3ea7cd27e416404b8341c060720149acd5a4
eofid <arhipadynec@gmail.com>
2024-10-25 14:54:19 +0300

    Update Contact.cpp

--- lab02/Source/Contact.cpp
+++ lab02/Source/Contact.cpp
@@ -4,8 +4,4 @@
 Contact::Contact(const std::string& name, const std::string& phone, const std::string& email)
     : name(name), phone(phone), email(email) {}
 
-// Реализация скрытого друга — оператора <<
-std::ostream& operator<<(std::ostream& os, const Contact& contact) {
-    os << contact.name << "\n" << contact.phone << "\n" << contact.email << "\n";
-    return os;
-}
+
